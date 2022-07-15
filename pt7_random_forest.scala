736e45928efe30d39a7b64b66efa5c936bbf2d99
Tassiane Barros <tassianebarrosti@gmail.com>
2022-07-15 16:29:47 -0300

    Salvando resultados

--- pt7_random_forest.scala
+++ pt7_random_forest.scala
@@ -7,6 +7,9 @@ import org.apache.spark.mllib.evaluation.MulticlassMetrics
 
 import spark.implicits._
 
+import java.io.File
+import java.io.PrintWriter
+
 
 // lê os dados do HDFS e converte para um dataframe
 val df = { 
@@ -14,7 +17,6 @@ val df = {
 	.format("parquet")
 	.load("hdfs://master:8020/bigdata/pt7-hash.parquet")
 }
-
 
 // //Indexando coluna de labels
 val labelIndexer = new StringIndexer()
@@ -37,14 +39,14 @@ val rf = new RandomForestClassifier()
 
 
 // Convert indexed labels back to original labels.
-val labelConverter = new IndexToString()
-  .setInputCol("prediction")
-  .setOutputCol("predictedLabel")
-  .setLabels(labelIndexer.labelsArray(0))
+// val labelConverter = new IndexToString()
+//   .setInputCol("prediction")
+//   .setOutputCol("predictedLabel")
+//   .setLabels(labelIndexer.labelsArray(0))  ///Taa dando erro aqui
 
 // Chain indexers and forest in a Pipeline.
 val pipeline = new Pipeline()
-  .setStages(Array(labelIndexer, rf, labelConverter))
+  .setStages(Array(labelIndexer, rf))
 
 //TODO: Remover caso nao utilizar CV
 // val paramGrid = new ParamGridBuilder()
@@ -64,23 +66,25 @@ val pipeline = new Pipeline()
 // Train model. This also runs the indexers.
 val model = pipeline.fit(trainingData)
 
+//////////Funcionando ate aqui/////////////////
+
+
 // Make predictions.
 val predictions = model.transform(testData)
 
 // Select example rows to display.
-predictions.select("predictedLabel", "label", "features").show(5)
-
+predictions.select("label", "features").show(5)
 
 //Funcao responsavel por calcular as metricas
-def cal_metrics(metric: String) : Double = {
-  val evaluator = new MulticlassClassificationEvaluator()
-    .setLabelCol("indexedLabel")
-    .setPredictionCol("prediction")
-    .setMetricName(metric)
+// def cal_metrics(metric: String, predictions: org.apache.spark.sql.Dataset[_]) : Double = {
+//   val evaluator = new MulticlassClassificationEvaluator()
+//     .setLabelCol("indexedLabel")
+//     .setPredictionCol("prediction")
+//     .setMetricName(metric)
 
-  val metric_value = evaluator.evaluate(predictions)
-  return metric_value
-}
+//   val metric_value = evaluator.evaluate(predictions)
+//   return metric_value
+// }
 
 
 
@@ -91,27 +95,99 @@ def cal_metrics(metric: String) : Double = {
 //weightedPrecision
 //recallByLabel
 //weightedRecall
-val metrics = Array("accuracy", "f1", "precisionByLabel", "weightedPrecision", "recallByLabel", "weightedRecall")
+// val metrics = Array("accuracy", "f1", "precisionByLabel", "weightedPrecision", "recallByLabel", "weightedRecall")
 
 //Calcular metricas 
-// foreach metrics
 
-
-// foreach(f : scala.Function1[T, scala.Unit]) : scala.Unit
 //Salvar em arquivo as métricas
 import scala.collection.mutable.ArrayBuffer
 val final_metrics  = ArrayBuffer[(String, Double)]()
 
-for(metric <- metrics){
-  print(metric)
-  // final_metrics = final_metrics :+ cal_metrics(metric)
-  final_metrics += ((metric, cal_metrics(metric)))
-}
+// for(metric <- metrics){
+//   print(metric)
+//   // final_metrics = final_metrics :+ cal_metrics(metric)
+//   final_metrics += ((metric, cal_metrics(metric, predictions )))
+// }
 
-//Salvar métrica no arquivo
+
+//select only prediction and label columns
+val preds_and_labels = predictions.select("prediction","indexedLabel")
+
+val metrics_ = new MulticlassMetrics(preds_and_labels.rdd.map(x => (x(0).asInstanceOf[Double], x(1).asInstanceOf[Double])))
+
+//Escrever metricas no arquivo
+val file_Object = new File("/user_data/metrics.txt" ) 
+val print_writer = new PrintWriter(file_Object) 
+
+// Confusion Matrix
+val confusionMatrixOutput = metrics_.confusionMatrix.toString
+// val confusionMatrixOutputFinal = spark.parallelize(confusionMatrixOutput)
+print_writer.write(s"ConfusionMatrix:\n$confusionMatrixOutput")
+print_writer.write("\n\n")
+
+// Labels
+val labels = metrics_.labels
+
+// False positive rate by label
+labels.foreach { l =>
+  print_writer.write(s"FPR($l) = " + metrics_.falsePositiveRate(l) + "\n")
+}
+print_writer.write("\n")
+
+// True positive rate by label
+labels.foreach { l =>
+  print_writer.write(s"TPR($l) = " + metrics_.truePositiveRate(l) + "\n")
+}
+print_writer.write("\n")
+
+// F-measure by label
+labels.foreach { l =>
+  print_writer.write(s"F1-Score($l) = " + metrics_.fMeasure(l) + "\n")
+}
+print_writer.write("\n")
+
+// Precision by label
+labels.foreach { l =>
+  print_writer.write(s"Precision($l) = " + metrics_.precision(l) + "\n")
+}
+print_writer.write("\n")
+
+// Recall by label
+labels.foreach { l =>
+  print_writer.write(s"Recall($l) = " + metrics_.recall(l) + "\n")
+}
+print_writer.write("\n")
+
+
+val accuracy = metrics_.accuracy
+val weightedFalsePositiveRate = metrics_.weightedFalsePositiveRate
+val weightedFMeasure = metrics_.weightedFMeasure
+val weightedPrecision = metrics_.weightedPrecision
+val weightedRecall = metrics_.weightedRecall
+val weightedTruePositiveRate = metrics_.weightedTruePositiveRate
+
+print_writer.write("Summary Statistics" + "\n")
+print_writer.write(s"Accuracy = $accuracy" + "\n")
+print_writer.write(s"weightedFalsePositiveRate = $weightedFalsePositiveRate" + "\n")
+print_writer.write(s"weightedFMeasure = $weightedFMeasure" + "\n")
+print_writer.write(s"weightedPrecision = $weightedPrecision" + "\n")
+print_writer.write(s"weightedRecall = $weightedRecall" + "\n")
+print_writer.write(s"weightedTruePositiveRate = $weightedTruePositiveRate" + "\n")
+
+
+
+
+// accuracy          falsePositiveRate   logLoss     truePositiveRate            weightedPrecision
+// confusionMatrix   hammingLoss         precision   weightedFMeasure            weightedRecall
+// fMeasure          labels              recall      weightedFalsePositiveRate   weightedTruePositiveRate
+
+
+// Closing printwriter    
+print_writer.close() 
+
 
 //Salvar o modelo no HDFS
-
+// model.save("hdfs://master:8020/bigdata/modelo_rf")
 
 
 
